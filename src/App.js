f9aa75685a6478a042ceaa1430cb0b6b5565252d
Your Name <Khalilcheguenni@gmail.com>
2021-01-03 11:36:23 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import img1 from "./source/img1.jpg";
+import img2 from "./source/img2.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default function App() {
+      return (
+        <div>
+          <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+            <h1 className="title red">Your name here</h1>
+            <br />
+            <img src={img1} />
+            <br />
+            <img src={img2} />
+          </div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/MQcXEWPesAw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      );
+      }
